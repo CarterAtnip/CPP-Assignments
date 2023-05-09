821e96d4d5ffbda301001ecc9257665bdd6c5e97
Carter <carter.atnip@gmail.com>
2023-05-08 22:03:54 -0500

    adding ints assignment

---
+++ adding_ints/adding_ints.cpp
@@ -0,0 +1,15 @@
+/*
+This program stores two integers in their respective variables, adds them and stores the total, and then prints
+to console
+ */
+#include <iostream>
+using namespace std;
+
+int main(){
+    int num1 = 62;
+    int num2 = 99;
+    int total = num1 + num2;
+
+    cout << "The sum of num1 and num2 is " << total << "\nCarter Atnip\a";
+    return 0;
+}
